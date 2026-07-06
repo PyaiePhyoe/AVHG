466ce2d02cdd0611cc0c68222f15136e19f3c883
Pyaie Phyoe <182846820+PyaiePhyoe@users.noreply.github.com>
2026-07-06 15:31:38 +0700

    modify sensor adding health, status, and list variables method

--- src/sensor.py
+++ src/sensor.py
@@ -4,7 +4,8 @@ from state_variable import State_Variable
 class Sensor(Component):
     def __init__(self, name, description):
         super().__init__(name, description)
-        
+        self.health = 100.00
+        self.status = "Normal"
         self.state_variables = []
     
     def add_state_variable(self, variable):
@@ -14,8 +15,12 @@ class Sensor(Component):
             )
         self.state_variables.append(variable)
 
-    def read(self):
-        return self.state_variables
+    def list_state_variables(self):
+        print(f"Variables of {self.name}")
+        print("---------------")
+        for state_variable in self.state_variables:
+            #return state_variable.name
+            print(f"{state_variable.name}: {state_variable.value} {state_variable.unit}\n")
     
     
 
